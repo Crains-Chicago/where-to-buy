304f92901a47b81ff474ef9fa81bf1c38ebecd42
Jean Cochrane <jean@jeancochrane.com>
2017-03-21 16:51:08 -0500

    Turn this into a Jekyll site

---
+++ js/wheretobuy.js
@@ -0,0 +1,53 @@
+var geocoder = new google.maps.Geocoder();
+
+var WhereToBuy = WhereToBuy || {};
+var WhereToBuy = {
+
+    // Map config
+    map: null,
+    map_centroid: [41.790, -87.636],
+    defaultZoom: 10,
+    lastClickedLayer: null,
+    legend: null,
+    locationScope: 'Chicago',
+
+    // Carto databases
+    community_areas: 'chicago_community_areas',
+
+    // Layer styles
+    boundaries: {
+        cartocss: $('#boundary-styles').html().trim(),
+        color: '#ffffcc',
+        opacity: 0.75
+    },
+
+    initialize: function() {
+
+        var google_map_styles = [
+            {
+                stylers: [
+                    { saturation: -100 },
+                    { lightness: 40 }
+                ]
+            }
+        ];
+
+        if (!WhereToBuy.map) {
+            // Initialize a Leaflet map
+            WhereToBuy.map = L.map('map', {
+                center: WhereToBuy.map_centroid,
+                zoom: WhereToBuy.defaultZoom,
+                dragging: true,
+                touchZoom: true,
+                zoomControl: true,
+                tap: true,
+                scrollWheelZoom: false
+            });
+        }
+
+        // Add streets to the map
+        WhereToBuy.streets = new L.Google('ROADMAP', {mapOptions: {styles: google_map_styles}});
+        WhereToBuy.map.addLayer(WhereToBuy.streets);
+    }
+
+};
