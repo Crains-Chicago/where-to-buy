3c99fd9a64da5201f6306c8deddd29a952758d96
Jean Cochrane <jean@jeancochrane.com>
2017-03-22 12:15:03 -0500

    More UI additions

--- js/wheretobuy.js
+++ js/wheretobuy.js
@@ -5,14 +5,21 @@ var WhereToBuy = {
 
     // Map config
     map: null,
-    map_centroid: [41.790, -87.636],
+    mapCentroid: [41.790, -87.636],
+    googleStyles: [{
+                        stylers: [
+                            { saturation: -100 },
+                            { lightness: 40 }
+                        ]
+                      }],
     defaultZoom: 8,
     lastClickedLayer: null,
     legend: null,
     locationScope: 'Chicago',
 
     // Carto databases
-    community_areas: 'chicago_community_areas',
+    chicagoBoundaries: 'chicago_community_areas',
+    suburbBoundaries: 'suburb_boundaries',
 
     // Layer styles
     boundaries: {
@@ -23,19 +30,10 @@ var WhereToBuy = {
 
     initialize: function() {
 
-        var google_map_styles = [
-            {
-                stylers: [
-                    { saturation: -100 },
-                    { lightness: 40 }
-                ]
-            }
-        ];
-
         if (!WhereToBuy.map) {
             // Initialize a Leaflet map
             WhereToBuy.map = L.map('map', {
-                center: WhereToBuy.map_centroid,
+                center: WhereToBuy.mapCentroid,
                 zoom: WhereToBuy.defaultZoom,
                 dragging: true,
                 touchZoom: true,
@@ -46,8 +44,41 @@ var WhereToBuy = {
         }
 
         // Add streets to the map
-        WhereToBuy.streets = new L.Google('ROADMAP', {mapOptions: {styles: google_map_styles}});
+        WhereToBuy.streets = new L.Google('ROADMAP', {mapOptions: {styles: WhereToBuy.googleStyles}});
         WhereToBuy.map.addLayer(WhereToBuy.streets);
+
+        // Query carto for boundaries
+        var fields = "";
+        var layerOpts = {
+            user_name: 'datamade',
+            type: 'cartodb',
+            sublayers: [{
+                sql: "SELECT * FROM" + WhereToBuy.chicagoBoundaries,
+                cartocss: WhereToBuy.boundaries.cartocss,
+                interactivity: fields
+            }, {
+                sql: "SELECT * FROM" + WhereToBuy.suburbBoundaries,
+                cartocss: WhereToBuy.boundaries.cartocss,
+                interactivity: fields
+            }]
+        };
+
+        var boundaries = cartodb.createLayer(WhereToBuy.map, layerOpts, { https: true },
+            function(layer) {
+
+                var sublayers = [];
+                var sub1 = layer.getSubLayer(0);
+                var sub2 = layer.getSublayer(1);
+                sublayers.push(sub1, sub2);
+
+                sublayers.forEach(function(sublayer) {
+                    sublayer.setInteraction(true);
+                    // Set more interactions here
+                });
+            }).on('done', function(layer) {
+                layer.addTo(WhereToBuy.map);
+            });
+
     }
 
 };
