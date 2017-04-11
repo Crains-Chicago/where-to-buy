315c555caecb0047dc392311033a465014519763
Jean Cochrane <jean@jeancochrane.com>
2017-04-11 10:45:53 -0500

    Scrape real estate PDFs from CAR

--- data/real_estate.mk
+++ data/real_estate.mk
@@ -1,10 +1,4 @@
-pdfs: scripts/secrets.py
-	mkdir -p raw/pdfs/
-	USER=$$(eval cat $< | grep MLS_USER | grep -Eo "'.*'")
-	PASS=$$(eval cat $< | grep MLS_PASS | grep -Eo "'.*'")
-	wget --save-cookies cookies.txt --keep-session-cookies --user=$$USER --password=$$PASS \
-		--delete-after http://car.stats.10kresearch.com/login? &&\
-	wget --load-cookies cookies.txt --no-use-server-timestamps -P raw/pdfs/ \
-		-r -nd -l1 -H -A rpt_* http://car.stats.10kresearch.com/docs/lmu/list
+pdfs: 
+	python scripts/retrieve_pdfs.py
 	touch $@
 
--- data/scripts/retrieve_pdfs.py
+++ data/scripts/retrieve_pdfs.py
@@ -1,20 +1,23 @@
+import os
+
 import requests
 import lxml.html
 
-from secrets import MLS_USER, MLS_PASS
+from secrets import CAR_USER, CAR_PASS
 
 
 class RequestException(Exception):
     pass
 
-LOGIN_URL = "http://car.stats.10kresearch.com/login"
-LIST_URL = "http://car.stats.10kresearch.com/docs/lmu/list"  # recent reports
+BASE_URL = "http://car.stats.10kresearch.com"
+LOGIN_URL = BASE_URL + "/login"
+LIST_URL = BASE_URL + "/docs/lmu/list"  # recent reports
 
 # Get the verification token
-raw_login = requests.get(LOGIN_URL).text
+raw_login = requests.get(LOGIN_URL)
 
-if raw_login.status_code == 200:
-    login_page = lxml.html.fromstring(raw_login)
+if raw_login.ok:
+    login_page = lxml.html.fromstring(raw_login.text)
 
     verification_div_id = "LocationList"
     dom_query = "//body/form/input[@name='__RequestVerificationToken']"
@@ -24,8 +27,8 @@ else:
                            % (raw_login.status_code))
 
 __RequestVerificationToken = target_input.value
-UserName = MLS_USER
-Password = MLS_PASS
+UserName = CAR_USER
+Password = CAR_PASS
 f = '1'
 
 payload = {
@@ -35,8 +38,36 @@ payload = {
     'f': f
 }
 
-# WIP
 with requests.Session() as s:
     p = s.post(LOGIN_URL, data=payload)
+    if p.ok:
+        r = s.get(LIST_URL)
+        if r.ok:
+            link_page = lxml.html.fromstring(r.text)
+            link_query = "//ul[@class='LocationList']/li/a"
+            links = link_page.xpath(link_query)
 
-    r = s.get(LIST_URL)
+            for link in links:
+                full_path = BASE_URL + link.attrib['href']
+                community_name = link.text
+                community_filename = community_name.replace(' ', '_') + '.pdf'
+                output_path = os.path.join('raw', 'pdfs', community_filename)
+
+                response = s.get(full_path)
+                if response.url != full_path:
+                    print('Redirect :(')
+                    print(response.content)
+                elif response.ok:
+                    with open(output_path, 'wb') as pdf:
+                        pdf.write(response.content)
+                        print('Downloaded %s from %s'
+                              % (output_path, full_path))
+                else:
+                    print("Couldn't load the file %s (status code: %s)"
+                          % (full_path, response.status_code))
+        else:
+            raise RequestException("Couldn't load the CAR list page (status code: %s)"
+                                   % (r.status_code))
+    else:
+        raise RequestException("Couldn't post login info (status code: %s)"
+                               % (p.status_code))
